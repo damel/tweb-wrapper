8b0b8c0e5d7b2e3428aad8898bdc6399b4131719
Dmitrii Amel <ameldmitrii@gmail.com>
2021-11-07 09:42:05 +0500

    init commit

---
+++ lib/tweb_wrapper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tweb_wrapper/version"
+
+module TwebWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tweb_wrapper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TwebWrapper
+  VERSION = "0.1.0"
+end
